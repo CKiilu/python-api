73a6081cfb496f9950493308df1af0ee23d07b26
CKiilu <cndeti@gmail.com>
2016-02-22 17:49:46 +0300

    Basic API for searching

--- api.py
+++ api.py
@@ -3,12 +3,21 @@
 import urllib2
 import json
 
-locu_api = 'e560a6caee0e2f04edf1d827a0cb1cb757ceb918'
 
-url = 'https://api.locu.com/v1_0/venue/search/?locality=Newport%20Beach&category=restaurant&api_key=e560a6caee0e2f04edf1d827a0cb1cb757ceb918'
+def locu_search(query):
+	locu_api = 'e560a6caee0e2f04edf1d827a0cb1cb757ceb918'
 
-json_obj = urllib2.urlopen(url)
+	api_key = locu_api
+	url = 'https://api.locu.com/v1_0/venue/search/?api_key=' + api_key
+	locality = query.replace(' ', '%20')
+	final_url = url + "&locality=" + locality + "&category=restaurant"
 
-data = json.load(json_obj)
 
-print data
+	json_obj = urllib2.urlopen(final_url)
+
+	data = json.load(json_obj)
+
+	for item in data['objects']:
+		print item['name'], item['phone']
+
+locu_search("San Francisco")
